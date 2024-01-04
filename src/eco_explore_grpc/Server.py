b36f566be589c6f56562bca207cd7a5da86c46f6
Obet3000 <zs20015717@estudiantes.uv.mx>
2024-01-04 12:27:47 -0600

    CreateExampleClient

--- src/eco_explore_grpc/Server.py
+++ src/eco_explore_grpc/Server.py
@@ -1,15 +1,26 @@
-import grpc 
+import grpc
+import comentarios_pb2
 import comentarios_pb2_grpc
-from concurrent import futures 
+from concurrent import futures
 
-def Server():
+class ComentariosService(comentarios_pb2_grpc.ComentariosServiceServicer):
+    def PublicarComentario(self, request, context):
+        return comentarios_pb2.Respuesta(exito=True, mensaje="Comentario publicado exitosamente")
+
+    def ObtenerComentarios(self, request, context):
+        comentarios = [
+            comentarios_pb2.Comentario(autor="Usuario1", contenido="¡Gran comentario!"),
+            comentarios_pb2.Comentario(autor="Usuario2", contenido="Otro comentario interesante"),
+        ]
+        return comentarios_pb2.ListaComentarios(comentarios=comentarios)
+
+def run_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    comentarios_pb2_grpc.add_ComentariosServiceServicer_to_server(comentarios_pb2_grpc.ComentariosServiceServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    comentarios_pb2_grpc.add_ComentariosServiceServicer_to_server(ComentariosService(), server)
+    server.add_insecure_port('localhost:50059')
     server.start()
-    print("Empezó")
+    print("Servidor gRPC iniciado en el puerto 50059")
     server.wait_for_termination()
-    print("Terminó")
 
 if __name__ == '__main__':
-    Server()
+    run_server()
