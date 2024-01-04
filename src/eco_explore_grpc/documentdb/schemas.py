0070daf419bc28fe6c619efc0a86de9e58c01224
Obet3000 <zs20015717@estudiantes.uv.mx>
2024-01-04 15:17:26 -0600

    ChangesConfig

--- src/eco_explore_grpc/documentdb/schemas.py
+++ src/eco_explore_grpc/documentdb/schemas.py
@@ -5,48 +5,7 @@ from pydantic_mongo import ObjectIdField
 from pydantic_extra_types.phone_numbers import PhoneNumber
 
 
-class EquipoNecesario(BaseModel):
-    Nombre: str
-    Cantidad: int = 0
-
-
-class PuntosInteres(BaseModel):
-    Lon: float
-    Lat: float
-    UrlMedia: Optional[List[str]]
-
-
 class Reseña(BaseModel):
     Evaluacion: int
     Comentario: str
 
-
-class Bitacora(BaseModel):
-    Nombre: str
-    Publica: bool
-    Descripcion: str
-    Actividad: str
-    Dificultad: str
-    EquipoNecesario: Optional[List[EquipoNecesario]]
-    PuntosInteres: List[PuntosInteres]
-    Comentarios: Optional[List[ObjectIdField]]
-
-
-class Usuarios(BaseModel):
-    Nombre: str
-    ApellidoPaterno: str
-    ApellidoMaterno: str
-    Email: EmailStr
-    UrlImagen: str
-    PerfilPublico: bool
-    Guia: bool
-    Telefono: PhoneNumber
-    Bitacoras: Optional[List[ObjectIdField]]
-
-
-class Exploraciones(BaseModel):
-    FechaAgendata: datetime
-    Guia: ObjectIdField
-    Exploradores: List[ObjectIdField]
-    Ruta: ObjectIdField
-    Precio: float
